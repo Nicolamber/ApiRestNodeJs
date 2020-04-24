dbfa07ed5d4ee8fa8febda58dd8e4ee15e6f8b48
Nico Lambertucci <nicolasmlambertucci@gmail.com>
2020-04-24 19:15:37 -0300

    Added json response need to improve the logic and also error handling

--- apiRest/routes/Clients.js
+++ apiRest/routes/Clients.js
@@ -3,67 +3,115 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'API REST NICO' });
 });
-
-router.route('/clients/:idClient/contracts')
-    .get(function(req,res){
-        res.send(`GET contracts from client with idClient = ${req.params.idClient}`)
-
-    })
-    .post(function(req,res){
-        res.send(`POST contract from client with idClient = ${req.params.idClient}`)
-
-    })
 
 router.route('/clients/:clientId/contracts/:contractId')
     .get(function(req,res){
-     res.send(`Hello from GET client ${req.params.clientId}  contract = ${req.params.contractId}`)
+     //res.send(`Hello from GET client ${req.params.clientId}  contract = ${req.params.contractId}`)
+     res.json({
+       status: 200,
+       message: "Hello from GET a specific contract of a specific client",
+       user: req.params.clientId,
+       contract: req.params.contractId //TODO igual aca, deberia llamar con este parametro al getContracts(idClient: String, idContract: String): Contract{}
+     })
     })
     .post(function(req,res){
-      res.send(`Hello from POST client ${req.params.clientId} contract = ${req.params.contractId}`)  
+      //res.send(`Hello from POST client ${req.params.clientId} contract = ${req.params.contractId}`)  
+      res.json({
+        status: 200,
+        message: "Hello from POST a specific contract of a specific client",
+        user: req.params.clientId,
+        contract: req.params.contractId //TODO nose si hace falta en un post pasar el contractId, salvo que sea para actualizar pero para eso esta el put
+      })
     })
     .put(function(req,res){
-      res.send(`Hello from PUT client ${req.params.clientId} contract = ${req.params.contractId}`) 
+      //res.send(`Hello from PUT client ${req.params.clientId} contract = ${req.params.contractId}`) 
+      res.json({
+        status: 200,
+        message: "Hello from PUT a specific contract of a specific client",
+        user: req.params.clientId,
+        contract: req.params.contractId //TODO igual aca, deberia llamar con este parametro al updateContract(contract: Contract): Boolean{}
     })
+  })
     .delete(function(req,res){
-      res.send(`Hello from DELETE client ${req.params.clientId} contract = ${req.params.contractId}`) 
+     // res.send(`Hello from DELETE client ${req.params.clientId} contract = ${req.params.contractId}`) 
+     res.json({
+      status: 200,
+      message: "Hello from DELETE a specific contract of a specific client",
+      user: req.params.clientId,
+      contract: req.params.contractId //TODO igual aca, deberia llamar con este parametro al deleteContract(id: String): Boolean{}
     })
+})
   
 router.route('/clients/:clientId/contracts')
     .get(function(req,res){
-      res.send(`Hello from GET all client contracts = ${req.params.clientId}`)
+        //res.send(`GET contracts from client with idClient = ${req.params.clientId}`)
+        res.json({
+          status: 200,
+          message: "Hello from GET all contracts of a specific client",
+          user: req.params.clientId,
+          contract: [1,5,2,6,55] //TODO aca deberia estar la llamada para obtener los contratos por cliente getContracts(id: String): List<Contracts>{}
+        })
     })
     .post(function(req,res){
-      res.send(`Hello from POST all client contracts = ${req.params.clientId}`)
-    })
-    .delete(function(req,res){
-      res.send(`Hello from DELETE all client contracts = ${req.params.clientId}`)
-    })
-    .put(function(req,res){
-      res.send(`Hello from PUT all client contracts = ${req.params.clientId}`)
+        //res.send(`POST contract from client with idClient = ${req.params.clientId}`)
+        res.json({
+          status: 200,
+          message: "Hello from POST all contracts of a specific client",
+          user: req.params.clientId,
+          contract: [1,5,2,6,55] //TODO NOSE SI HACE FALTA UN POST ACA, PREGUNTAR
+        })
     })
 
-    router.route('/clients/:clientId')
+router.route('/clients/:clientId')
     .get(function(req,res){
-      res.send(`Hello from GET a client= ${req.params.clientId}`)
+      //res.send(`Hello from GET a client= ${req.params.clientId}`)
+      res.json({
+        status: 200,
+        message: "Hello from GET a specific client",
+        user: req.params.clientId,
+        contract: req.params.contractId //TODO igual aca, deberia llamar con este parametro al getClient(id: String): Client{}
+      })
     })
     .delete(function(req,res){
-      res.send(`Hello from DELETE a client= ${req.params.clientId}`)
+      //res.send(`Hello from DELETE a client= ${req.params.clientId}`)
+      res.json({
+        status: 200,
+        message: "Hello from DELETE a specific client",
+        user: req.params.clientId,
+        contract: req.params.contractId //TODO igual aca, deberia llamar con este parametro al deleteClient(id: String): Boolean{}
+      })
     })
 
 router.route('/clients')
     .get(function(req,res){
-      res.send(`Hello from GET clients`)
+      //res.send(`Hello from GET clients`)
+      res.json({
+        status: 200,
+        message: "Hello from GET all clients"
+      })
     })
     .post(function(req,res){
-      res.send(`Hello from POST clients`)  
+      //res.send(`Hello from POST clients`)  
+      res.json({
+        status: 200,
+        message: "Hello from POST all clients"
+      })
     })
     .put(function(req,res){
-      res.send(`Hello from PUT clients`) 
+     // res.send(`Hello from PUT clients`) 
+     res.json({
+      status: 200,
+      message: "Hello from PUT all clients"
+    })
     })
     .delete(function(req,res){
-      res.send(`Hello from DELETE clients`) 
+     // res.send(`Hello from DELETE clients`) 
+     res.json({
+      status: 200,
+      message: "Hello from DELETE all clients"
+    })
     })
 
 module.exports = router;
